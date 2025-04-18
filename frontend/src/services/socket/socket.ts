a99b9cf8940bdac4831a5941ad41410fdbe63b44
samadou <mossabm036@gmail.com>
2025-04-18 20:39:34 +0100

    socket url mode

--- frontend/src/services/socket/socket.ts
+++ frontend/src/services/socket/socket.ts
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const io_url = import.meta.env.DEV
+  ? "http://localhost:3000"
+  : "https://echo-board-oqis.onrender.com";
+// const socket = io("http://localhost:3000");
+const socket = io(io_url);
 
 export default socket;
