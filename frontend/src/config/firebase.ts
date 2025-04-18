bc9273419439cca52928bb4eb5d405c91a8f2f9f
samadou <mossabm036@gmail.com>
2025-04-18 20:52:02 +0100

    added config firebase frontend

---
+++ frontend/src/config/firebase.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // Import Firebase Auth
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQXNrgsBr7_SGMb-yz04wKcF8SCFjTS-g",
+  authDomain: "echoboard-1f6a6.firebaseapp.com",
+  projectId: "echoboard-1f6a6",
+  storageBucket: "echoboard-1f6a6.firebasestorage.app",
+  messagingSenderId: "643786856276",
+  appId: "1:643786856276:web:e6d56b1153ba5bf656dac1",
+  measurementId: "G-W6HM4D3Z2V",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
