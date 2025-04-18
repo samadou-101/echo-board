694421ad7da43564c2f9668baf0002a7c1d11bfe
samadou <mossabm036@gmail.com>
2025-04-18 18:20:30 +0100

    firebase login

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { ServiceAccount } from "firebase-admin";
-import * as serviceAccount from "../../serviceAccountKey.json"; // adjust path if needed
+import * as serviceAccount from "../../serviceAccountKey.json";
 
 const app = !getApps().length
   ? initializeApp({
